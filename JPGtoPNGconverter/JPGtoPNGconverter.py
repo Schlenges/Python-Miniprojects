4442c4e0991ddd5fcef0562946d7702902bc1377
Schlenges <meike_hankewicz@yahoo.de>
2020-05-15 14:08:10 +0200

    add error handling for non-image files

--- JPGtoPNGconverter/JPGtoPNGconverter.py
+++ JPGtoPNGconverter/JPGtoPNGconverter.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 image_folder = sys.argv[1]
 output_folder = sys.argv[2]
@@ -9,8 +9,11 @@ if not os.path.exists(output_folder):
   os.makedirs(output_folder)
 
 for filename in os.listdir(image_folder):
-  img = Image.open(f'{image_folder}/{filename}')
-  clean_filename = os.path.splitext(filename)[0]
-  img.save(f'{output_folder}/{clean_filename}.png', 'png')
-  print(f'{filename} converted')
+  try:
+    img = Image.open(f'{image_folder}/{filename}')
+    clean_filename = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}/{clean_filename}.png', 'png')
+    print(f'{filename} converted')
+  except UnidentifiedImageError:
+    continue
 

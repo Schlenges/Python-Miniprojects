8875b846173ce0c6d585f869170aeadd478fb19c
Schlenges <meike_hankewicz@yahoo.de>
2020-05-15 16:23:07 +0200

    refactor step 1

--- JPGtoPNGconverter/JPGtoPNGconverter.py
+++ JPGtoPNGconverter/JPGtoPNGconverter.py
@@ -5,25 +5,33 @@ from PIL import Image, UnidentifiedImageError
 input_path = sys.argv[1]
 output_path = sys.argv[2]
 
+def _trim(filename):
+  trimmed = os.path.splitext(filename)[0].split('/')[-1]
+  return trimmed
+
+def _convert(input, output):
+  img = Image.open(input)
+  img.save(f'{output}.png', 'png')
+  print(f'{input} converted')
+
+
 if not os.path.exists(output_path):
   os.makedirs(output_path)
 
 if os.path.isdir(input_path):
   for filename in os.listdir(input_path):
     try:
-      img = Image.open(f'{input_path}/{filename}')
-      clean_filename = os.path.splitext(filename)[0]
-      img.save(f'{output_path}/{clean_filename}.png', 'png')
-      print(f'{filename} converted')
+      input_file = f'{input_path}/{filename}'
+      output_file = f'{output_path}/{_trim(filename)}'
+      _convert(input_file, output_file)
     except UnidentifiedImageError:
       continue
-
-if os.path.isfile(input_path):
+elif os.path.isfile(input_path):
   try:
-      img = Image.open(f'{input_path}')
-      clean_filename = os.path.splitext(input_path)[0]
-      clean_filename = clean_filename.split('/')[-1]
-      img.save(f'{output_path}/{clean_filename}.png', 'png')
-      print(f'{input_path} converted')
+    input_file = f'{input_path}'
+    output_file = f'{output_path}/{_trim(input_path)}'
+    _convert(input_file, output_file)
   except UnidentifiedImageError:
     print('file is not an image')
+else:
+  print('please enter a valid input file path')

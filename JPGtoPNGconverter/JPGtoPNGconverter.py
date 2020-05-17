a6937d85503f12c8c0f36636aaf40ca7a9343a79
Schlenges <meike_hankewicz@yahoo.de>
2020-05-17 09:31:13 +0200

    add main function

--- JPGtoPNGconverter/JPGtoPNGconverter.py
+++ JPGtoPNGconverter/JPGtoPNGconverter.py
@@ -6,35 +6,37 @@ def _trim(filename):
   trimmed = os.path.splitext(filename)[0].split('/')[-1]
   return trimmed
 
-def _convert(file_path):
+def _convert(file_path, output_path):
   img = Image.open(file_path)
-  output_path = f'{png_path}/{_trim(file_path)}'
+  output_path = f'{output_path}/{_trim(file_path)}'
   img.save(f'{output_path}.png', 'png')
   print(f'{file_path} converted')
 
-def run():
+def run(jpg_path, png_path):
   if not os.path.exists(png_path):
     os.makedirs(png_path)
 
   if os.path.isdir(jpg_path):
     for filename in os.listdir(jpg_path):
       try:
-        _convert(f'{jpg_path}/{filename}')
+        _convert(f'{jpg_path}/{filename}', png_path)
       except UnidentifiedImageError:
         continue
   elif os.path.isfile(jpg_path):
     try:
-      _convert(jpg_path)
+      _convert(jpg_path, png_path)
     except UnidentifiedImageError:
       print('file is not an image')
   else:
     print('please enter a valid input file path')
 
-try:
-  jpg_path = sys.argv[1]
-  png_path = sys.argv[2]
-  run()
-except IndexError:
-  print('please enter an input and output path')
-  sys.exit()
+def main(args):
+  try:
+    jpg_path = args[1]
+    png_path = args[2]
+    run(jpg_path, png_path)
+  except IndexError:
+    print('please enter an input and output path')
+    sys.exit()
 
+main(sys.argv[:3])

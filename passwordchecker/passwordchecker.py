d970b8bac691f2a10266da724f080b3f8894ec54
Schlenges <meike_hankewicz@yahoo.de>
2020-05-18 08:54:55 +0200

    add pep8 formatting

--- passwordchecker/passwordchecker.py
+++ passwordchecker/passwordchecker.py
@@ -2,38 +2,44 @@ import requests
 import hashlib
 import sys
 
+
 def request_data(query_chars):
-  url = f'https://api.pwnedpasswords.com/range/{query_chars}'
-  response = requests.get(url)
-  if response.status_code != 200:
-    raise RuntimeError(f'Error fetching data: status {response.status_code}, please check the api and try again')
-  return response
+    url = f'https://api.pwnedpasswords.com/range/{query_chars}'
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise RuntimeError(
+            f'Error fetching data: status {response.status_code}, please check the api and try again')
+    return response
+
 
 def get_leaks_count(hashes, chars_to_check):
-  lines = hashes.text.splitlines()
-  hashes = (line.split(':') for line in lines) # not actually a comprehension, but a generator expression
-  for h, count in hashes:
-    if h == chars_to_check:
-      return count
-  return 0
+    lines = hashes.text.splitlines()
+    hashes = (line.split(':') for line in lines)
+    for h, count in hashes:
+        if h == chars_to_check:
+            return count
+    return 0
+
 
 def check_pw(password):
-  pw_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-  query_chars, chars_to_check = pw_hash[:5], pw_hash[5:]
-  response = request_data(query_chars)
-  return get_leaks_count(response, chars_to_check)
-  
+    pw_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    query_chars, chars_to_check = pw_hash[:5], pw_hash[5:]
+    response = request_data(query_chars)
+    return get_leaks_count(response, chars_to_check)
+
+
 def main(arg):
-  try:
-    with open(arg) as pw_file:
-      for password in (line.strip() for line in pw_file):
-        count = check_pw(password)
-        if count:
-          print(f'{password} was found {count} times')
-        else:
-          print(f'{password} has not been found')
-  except FileNotFoundError as err:
-    print('file does not exist')
-    raise err
+    try:
+        with open(arg) as pw_file:
+            for password in (line.strip() for line in pw_file):
+                count = check_pw(password)
+                if count:
+                    print(f'{password} was found {count} times')
+                else:
+                    print(f'{password} has not been found')
+    except FileNotFoundError as err:
+        print('file does not exist')
+        raise err
+
 
 main(sys.argv[1])
